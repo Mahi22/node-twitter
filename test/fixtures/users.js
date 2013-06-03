6c7ecc8a73340d9eaeb0fd9c4400ee239548472c
vinit kumar <vinit.kumar@changer.nl>
2013-06-03 13:21:26 +0530

    starting grunt

--- test/fixtures/users.js
+++ test/fixtures/users.js
@@ -1,5 +1,32 @@
-exports.userA = {
+exports.userB = {
+  id: '106059522759137',
+  name: 'James Hussels',
+  email: 'madhu@qed42.com',
+  username: 'jhussels',
+  facebook: '',
+  provider: 'facebook',
+  accessToken: 'AAAFKvZB2v3GwBAG5EUs4ao2KRumvJU945QKoKsto6IeIZAAMDiYLhbl5h8bP8Sm3amMevxLQS5LH1OjQ8gPch2kRk4WFZAlEsIZCfej0nQZDZD',
+  refreshToken: ''
 }
 
-exports.userB = {\
+exports.userC = {
+  id: '132729080097791',
+  name: 'Tom Botman',
+  email: 'neokya@gmail.com',
+  username: 'tom.botman.7',
+  facebook: '',
+  provider: 'facebook',
+  accessToken: 'AAAFKvZB2v3GwBAGUFHrIDM5WwAsV0cuH7owP2Y4BMdnS3LZBEkuQKQ7XyiXtIhBvXxiNHv3Y5ZBxocDWNm8cocTt3EmYk38iZCMheG4aDgZDZD',
+  refreshToken: ''
+}
+
+exports.userA = {
+  id: '112969428713061',
+  name: 'Madhusudhan Srinivasa',
+  email: 'madhums8@gmail.com',
+  username: 'madhums',
+  facebook: '',
+  provider: 'facebook',
+  accessToken: 'AAAFKvZB2v3GwBAPhyTeDRNpf2E15ZBxXMgHLFuMIe0ZAlUui2CZAfbj1JoVPzz0LUSDZCuAxmGWTtjCS3lxODJ8UBZB6TLfCasP0xpObZANRAZDZD',
+  refreshToken: ''
 }
